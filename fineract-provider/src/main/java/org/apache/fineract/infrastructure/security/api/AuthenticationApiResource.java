f476aa451a42544a69f32204d7239518c6bb4349
Pareekshith1 <paripalat@gmail.com>
2025-08-06 12:35:02 +0530

    Enabled encrypted authentication for the pr creation

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/security/api/AuthenticationApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/security/api/AuthenticationApiResource.java
@@ -115,8 +115,8 @@ public class AuthenticationApiResource {
         }
 
 
-        //request.password = rsaEncryptionUtils.decryptUsingRSA(request.password,
-                    //encryptionKeyStoreService.retrieveKey(AUTH).getPrivateKey(), true);
+        request.password = rsaEncryptionUtils.decryptUsingRSA(request.password,
+                    encryptionKeyStoreService.retrieveKey(AUTH).getPrivateKey(), true);
 
         AppUser appUser = this.springSecurityPlatformSecurityContext.getAppUserByUsername(request.username);
 
